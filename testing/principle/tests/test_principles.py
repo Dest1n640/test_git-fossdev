3277d60c3fd2b6eadcad64cb1bd8d3e6c09f7ed5
Dest1n640 <realmadridfunca@gmail.com>
2026-03-05 13:04:49 +0800

    Added demo for do-not-duplicate-logic-tests

--- testing/principle/tests/test_principles.py
+++ testing/principle/tests/test_principles.py
@@ -9,11 +9,19 @@ def test_addition():
   print("Test ADDITION PASSED")
 
 def test_addition_with_bug():
+  # Тесты показывают наличие ошибок, а не их отсуствие
   assert add_with_bug(2, 2) == 4
   assert add_with_bug(0, 0) == 0
   # assert add_with_bug(7, 6) == 13 # Вызовиться ошибка
   print("Test BUGGED ADDITION PASSED")
 
+def test_addition_duplicate():
+  # Тесты не предсказывают работу проверяемой функции
+  assert add(6, 7) == 6 + 7
+  print("Test DUPLICATE ADDITION PASSED")
+
+
 if __name__ == "__main__":
   test_addition()
   test_addition_with_bug()
+  test_addition_duplicate()
